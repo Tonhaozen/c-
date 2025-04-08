4da450bd3ddca5b6a032864c5edc8d86b97e9573
TonhaoZEN <aanthonnytavares1@gmail.com>
2025-04-07 22:27:42 -0300

    Add files via upload

---
+++ aulaabimas/aulaabimas/aulaabimas/Program.cs
@@ -0,0 +1,33 @@
+﻿/* // See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(texto) */
+
+/*
+int a;
+int b;
+int c;
+int s;
+a = 10;
+b = 20;
+c = 30;
+s = 40;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine("resultado"+(a+b+c+s));
+Console.WriteLine(s);
+Console.WriteLine();
+*/
+
+string s;
+int a;
+int b;
+Console.WriteLine("digite um numero inteiro");
+s = Console.ReadLine();
+a = Convert.ToInt32(s);
+Console.WriteLine("valor de a e");
+Console.WriteLine(a);
+
+
+
+    
